eb2790cacce8545351c3728a3a0882d2a89d1354
skeltojo <skeltojo@aim.com>
2014-01-23 12:37:47 -0800

    Initial commit

---
+++ src/com/g08/sonder/Feed.java
@@ -0,0 +1,20 @@
+package com.g08.sonder;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Feed extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_feed, menu);
+        return true;
+    }
+}
