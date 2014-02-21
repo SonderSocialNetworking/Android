a4484ae33c5391b0196e792d50c804c42c59db89
skeltojo <skeltojo@aim.com>
2014-02-20 19:09:14 -0800

    Several more changes: 1)The most recent post is shown as a hint in the 'make post' field
    2)Reduced some of the lag in switching between fragments. There's still some lag, but not as bad
    
    3)Users can choose profile pics by clicking on the current profile pic. This pulls up the gallery on the phone.
      --Known error: pictures chosen from external places (Dropbox, Picasso, etc.) cause the app to crash.

--- src/com/g08/sonder/ProfileFragment.java
+++ src/com/g08/sonder/ProfileFragment.java
@@ -1,12 +1,24 @@
 package com.g08.sonder;
  
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +26,12 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
  
 public class ProfileFragment extends Fragment {
+	ParseUser user = ParseUser.getCurrentUser();
  
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,7 +51,7 @@ public class ProfileFragment extends Fragment {
 		TextView ageText = (TextView) rootView.findViewById(R.id.ProfileAge);
 		
 		
-		ParseUser user = ParseUser.getCurrentUser();
+		//ParseUser user = ParseUser.getCurrentUser();
 		
 		//Convert the user's full name to a char array for use in the TextView
 		String userNameString = user.getString("FirstName") + " " + user.getString("LastName");
@@ -85,6 +99,53 @@ public class ProfileFragment extends Fragment {
 			
 		});*/
 		
+		//Get the imageButton for the profile Pic
+		final ImageButton profPic = (ImageButton) rootView.findViewById(R.id.ProfileActivityPhoto);
+		
+		
+		ParseFile file;
+		file = user.getParseFile("Pic");
+		if(file!=null)
+		{
+		
+			byte[] bytesArray = null;
+			try {
+				bytesArray = file.getData();
+			} catch (ParseException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+		
+			
+		
+			Bitmap bmp = BitmapFactory.decodeByteArray(bytesArray, 0, bytesArray.length);
+		
+			profPic.setImageBitmap(bmp);
+		
+		}
+		profPic.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+				photoPickerIntent.setType("image/*");
+				startActivityForResult(photoPickerIntent, 1); 
+				
+				
+				
+				
+				
+			}
+			
+			
+			
+		});
+		
+		EditText textField = (EditText) rootView.findViewById(R.id.makePostText);
+		textField.setHint(user.getString("AnonPost"));
+		
+		
 		sendMessageButton = (Button) rootView.findViewById(R.id.sendMessage);
 		sendMessageButton.setOnClickListener(new OnClickListener(){
 
@@ -93,7 +154,7 @@ public class ProfileFragment extends Fragment {
 				EditText postField = (EditText) getView().findViewById(R.id.makePostText);
 				String postText = postField.getText().toString();
 				
-				ParseUser user = ParseUser.getCurrentUser();
+				//ParseUser user = ParseUser.getCurrentUser();
 				user.put("AnonPost", postText);
 				
 				user.saveInBackground(new SaveCallback() {
@@ -116,6 +177,8 @@ public class ProfileFragment extends Fragment {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
+				Toast.makeText(v.getContext(), "You've made a post!",
+					      Toast.LENGTH_SHORT).show();
 			}
 			
 		});
@@ -130,7 +193,79 @@ public class ProfileFragment extends Fragment {
             }
         }); */
         return rootView;
-      }    
+      }
+    
+    public void onActivityResult(int requestCode, int resultCode, 
+    	       Intent imageReturnedIntent) {
+    	    super.onActivityResult(requestCode, resultCode, imageReturnedIntent); 
+
+    	    switch(requestCode) { 
+    	    case 1:
+    	    	
+    	        if(resultCode == Activity.RESULT_OK){  
+    	        	Log.d("1","Check here");
+    	            Uri selectedImage = imageReturnedIntent.getData();
+    	            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+    	            Cursor cursor = getActivity().getContentResolver().query(
+    	                               selectedImage, filePathColumn, null, null, null);
+    	            cursor.moveToFirst();
+
+    	            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+    	            String filePath = cursor.getString(columnIndex);
+    	            cursor.close();
+
+
+    	            Bitmap profSelectedImage = BitmapFactory.decodeFile(filePath);
+    	            
+    	            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    	            profSelectedImage.compress(Bitmap.CompressFormat.JPEG, 20, stream);
+    	            byte[] data = stream.toByteArray();
+    	            
+    	            ParseFile picFile = new ParseFile(data);
+    	            picFile.saveInBackground(new SaveCallback() {
+  					   public void done(com.parse.ParseException e) {
+   					     if (e == null) {
+   			
+   					       Log.d("1", "Picture saved");
+   					       
+   					       
+   					     } else {
+   					    	 Log.d("1","Picture did not save");
+   					    	 
+   					     }
+   					   }
+
+   					
+   					 });
+    	            
+    	            //ParseUser user = ParseUser.getCurrentUser();
+    	            
+    	            user.put("Pic", picFile);
+    	            user.saveInBackground(new SaveCallback() {
+ 					   public void done(com.parse.ParseException e) {
+ 					     if (e == null) {
+ 			
+ 					       Log.d("1", "Picture saved to profile");
+ 					       
+ 					       
+ 					     } else {
+ 					    	 Log.d("1","Picture not on profile");
+ 					    	 
+ 					     }
+ 					   }
+
+ 					
+ 					 });
+    	            try {
+						user.refresh();
+					} catch (ParseException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+    	        }
+    	    }
+    	}
         
     
 }
